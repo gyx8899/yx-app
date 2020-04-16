e0e74a526174c37e1d0ab1d07927676ef3074bff
Steper Kuo <gyx8899@126.com>
2020-04-16 15:27:19 +0800

    refactor(_app): Move common header to _document

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from 'next/app';
 import Head from 'next/head';
 import Router from 'next/router';
-import {site, author} from '../site/config';
+import {site} from '../site/config';
+
+const {title} = site.header;
 
 import PageChange from '../app/components/PageChange/PageChange.js';
 
@@ -25,10 +27,6 @@ Router.events.on('routeChangeError', () => {
   ReactDOM.unmountComponentAtNode(document.getElementById('page-transition'));
   document.body.classList.remove('body-page-transition');
 });
-
-const {
-title, description, keywords, themeColor, gaTrackingId,
-} = site.header;
 
 export default class MyApp extends App {
   componentDidMount() {
@@ -52,26 +50,6 @@ export default class MyApp extends App {
       <>
         <Head>
           <title>{title}</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta charSet="utf-8" />
-          <meta name="description" content={description} />
-          <meta name="keywords" content={keywords} />
-          <meta name="author" content={author.name} />
-          <link rel="shortcut icon" href="/images/favicon.ico" type="image/ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-icon-180x180.png" />
-          <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon-96x96.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="msapplication-TileColor" content={themeColor} />
-          <meta name="msapplication-TileImage" content="/images/ms-icon-144x144.png" />
-          <meta name="msapplication-config" content="/browserconfig.xml" />
-          {/* {<!-- Status Bar Style -->} */}
-          {/* {<!-- Safari: black, black-translucent -->} */}
-          <meta name="apple-mobile-web-app-status-bar-style" content="yes" />
-          {/* {<!-- Chrome, Firefox OS and Opera -->} */}
-          <meta name="theme-color" content={themeColor} />
-          {
-            gaTrackingId && <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`} />
-          }
         </Head>
         <Component {...pageProps} />
       </>
